530c19e4a0268b4375cfd7b95ffb63b084b1abca
Dusan Zatkovsky <dusanmsk@gmail.com>
2017-01-18 22:42:28 +0100

    total encapsulation of shiftout class

--- relayduino/code/sketches/shiftLearn/shiftLearn.ino
+++ relayduino/code/sketches/shiftLearn/shiftLearn.ino
@@ -1,14 +1,14 @@
 
-    int pinData = 53;
-    int pinOutputEnable = 51;
-    int pinReset = 45;
-    int pinShiftClock = 47;
-    int pinLatchClock = 49;
-
 
 class ShiftOut {
 
   private:
+    int pinData ;
+    int pinOutputEnable ;
+    int pinReset ;
+    int pinShiftClock ;
+    int pinLatchClock ;
+
 
     void resetShiftRegister() {
       digitalWrite(pinReset, LOW);
@@ -16,7 +16,30 @@ class ShiftOut {
     }
 
   public:
-  
+
+    ShiftOut(int pinData, int pinOutputEnable, int pinReset, int pinShiftClock, int pinLatchClock) {
+      pinMode(pinData, OUTPUT);
+      digitalWrite(pinData, LOW);
+      
+      pinMode(pinReset, OUTPUT);
+      digitalWrite(pinReset, HIGH);             // HIGH = do nothing, LOW = reset shift register
+      
+      pinMode(pinOutputEnable, OUTPUT);
+      digitalWrite(pinOutputEnable, LOW);       // LOW = enable output, pass data from latch register to outputs, HIGH = do nothing
+      
+      pinMode(pinShiftClock, OUTPUT);
+      digitalWrite(pinShiftClock, LOW);
+    
+      pinMode(pinLatchClock, OUTPUT);
+      digitalWrite(pinLatchClock, LOW);
+      
+      this->pinData = pinData;
+      this->pinOutputEnable = pinOutputEnable;
+      this->pinReset = pinReset;
+      this->pinShiftClock = pinShiftClock;
+      this->pinLatchClock = pinLatchClock;
+    }
+    
     void send(int data[], int arrLen) {
       // set everything to low
       digitalWrite(pinShiftClock, LOW);
@@ -58,73 +81,15 @@ void dbg(String msg) {
   Serial.println(msg);
 }
 
-/*
-void shiftOut(byte data) {
-  bool pinState = false;
-
-  // set everything to low
-  digitalWrite(pinShiftClock, LOW);
-  digitalWrite(pinLatchClock, LOW);
-  digitalWrite(pinData, LOW);
-
-  // reset shift register(s)
-  digitalWrite(pinReset, LOW);
-  digitalWrite(pinReset, HIGH);
-
-  // send data, tick with the shift clock for each bit
-  for (int i=7; i>=0; i--)  {
-    pinState = ( data & (1<<i) );
-    // write data
-    digitalWrite(pinData, pinState);
-
-    // tick with shift register clock
-    digitalWrite(pinShiftClock, LOW);
-    digitalWrite(pinShiftClock, HIGH);
-    digitalWrite(pinShiftClock, LOW);
-
-    // reset data pin to zero
-    digitalWrite(pinData, LOW);
-    
-    //delay(10*spd);    
-  }
-
-  // now data is in the shift register. To "show" them, tick with latch clock and all data will be "copied" from shift register to latch register
-  digitalWrite(pinLatchClock, LOW);
-  digitalWrite(pinLatchClock, HIGH);
-  digitalWrite(pinLatchClock, LOW);
-
-  // enable output (this normally does nothing because output is permanently enabled)
-  digitalWrite(pinOutputEnable, LOW);
-  
-  dbg("done");
-}
-*/
-
-ShiftOut out;
-
+ShiftOut out(53,51,45,47,49);
 void setup() {
-
-  out = ShiftOut();
-
   Serial.begin(9600);
-  
-  // put your setup code here, to run once:
-  pinMode(pinData, OUTPUT);
-  digitalWrite(pinData, LOW);
-  
-  pinMode(pinReset, OUTPUT);
-  digitalWrite(pinReset, HIGH);             // HIGH = do nothing, LOW = reset shift register
-  
-  pinMode(pinOutputEnable, OUTPUT);
-  digitalWrite(pinOutputEnable, LOW);       // LOW = enable output, pass data from latch register to outputs, HIGH = do nothing
-  
-  pinMode(pinShiftClock, OUTPUT);
-  digitalWrite(pinShiftClock, LOW);
+}
 
-  pinMode(pinLatchClock, OUTPUT);
-  digitalWrite(pinLatchClock, LOW);
-  
-
+void loop1() {
+  int d1[] = {1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1};
+  out.send(d1,16);
+  delay(5000000);
 }
 
 void loop() {
