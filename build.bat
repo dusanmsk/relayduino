cee062270d508d87def50d6035ab3b9bd5f8ccd6
Dusan Zatkovsky <dusanmsk@gmail.com>
2017-12-17 12:25:18 +0100

    another renames

--- build.bat
+++
@@ -1,5 +0,0 @@
-cd code
-pio run
-cp .pioenvs/nanoatmega328/firmware.hex ../induino_firmware_nano328.hex
-cp .pioenvs/megaatmega2560/firmware.hex ../induino_firmware_mega2560.hex
-git add induino_firmware*.hex
