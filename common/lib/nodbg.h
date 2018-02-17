634604b0b69f1aa7d2fe419e5dacf1d9647640e1
Dusan Zatkovsky <dusanmsk@gmail.com>
2018-02-17 17:37:10 +0100

    ...

--- common/lib/nodbg.h
+++
@@ -1,9 +0,0 @@
-#ifndef DBG_H_
-#define DBG_H_
-
-#define dbg(x)
-#define dbgf(msg, arg1)
-#define dbgf2(msg, arg1, arg2)
-
-
-#endif
--- tester/src/outputBoardTest.py
+++
@@ -1,5 +0,0 @@
-from socket import *
-cs = socket(AF_INET, SOCK_DGRAM)
-cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-cs.sendto('This is a test', ('255.255.255.255', 6666))
